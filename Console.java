0fcbdc1e9db9bcc7269a41387b288dd17d9b8931
KatsuMouley <vitorluizsantosmougenot@gmail.com>
2024-03-14 22:45:54 -0300

    Small formatting correction

--- Console.java
+++ Console.java
@@ -4,6 +4,7 @@ class Console{
         Ex2 SecondExercise = new Ex2();
         Ex3 ThirdExercise = new Ex3();
         //Exercise 1
+        System.out.println("-------------/Exercise 1/-------------");
         FirstExercise.PrintEx1();
     }
 }

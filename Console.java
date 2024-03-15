6ada4589fe401368fb1a976df049fcb953df3b78
KatsuMouley <vitorluizsantosmougenot@gmail.com>
2024-03-14 22:19:12 -0300

    Setting Up ambient

---
+++ Console.java
@@ -0,0 +1,8 @@
+class Console{
+    //Exercício de fixação 1
+    public static void main(String[] args) {
+        Ex1 FirstExercise = new Ex1();
+        Ex2 SecondExercise = new Ex2();
+        Ex3 ThirdExercise = new Ex3();
+    }
+}

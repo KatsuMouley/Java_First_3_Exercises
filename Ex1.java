baf28429464dff3778c3a2967fb021984b05d4d7
KatsuMouley <vitorluizsantosmougenot@gmail.com>
2024-03-14 22:43:03 -0300

    Exercise 1 comment

--- Ex1.java
+++ Ex1.java
@@ -1,10 +1,12 @@
 public class Ex1 {
     void PrintEx1(){
+        //On this exercise our goal is to create an program that process the following data
         int matricula = 1;
         String name = "Katsu Mouley";
         double nota1 = 9.5;
         double nota2 = 9.5;
         double notaFinal = (nota1 + nota2) / 2;
+        //And our next step is to print it
         System.out.printf("Matricula: %05d\n", matricula);
         System.out.printf("Nome: %s\n", name);
         if (notaFinal > 6) {
